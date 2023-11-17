22ca8e2208fcd0a3086a4630960f957f6f3e2aa7
DHRUV GAUTAM <143205326+gautamdhruv-08@users.noreply.github.com>
2023-11-17 14:27:56 +0000

    disscusson on data type

---
+++ bascies1/data-type.js
@@ -0,0 +1,36 @@
+// Primitive  (Stack)
+/*
+String , Number , Null, Boolean , Undefined ,symbol , BigInt */
+
+const firstname = "Dhruv"
+const age = 20
+const score = 200
+let isLoggedIn = true
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = Symbol("id")
+const aReallyLongNumber = 89756481254625855n
+console.log(score);
+/* Refernce (Non Primitive) 
+ 1. Arrays 
+ 2. Objects 
+ 3. Functions 
+ */
+const numbers = [1,2,3,4,5,6]
+
+const heroes = [
+    "Aman",
+    "NAveen",
+    "harshit",
+    " Saktiman"
+    ]
+    const userObject = {
+        name:"HCL",
+        age:25,
+        isLoggedIn:true
+
+    }
+
+    const sayHello = function(){
+        console.log(age);
+    }
