3fb58bf5b96f354011d1b636cad49d2ef0473871
DHRUV GAUTAM <143205326+gautamdhruv-08@users.noreply.github.com>
2024-01-09 13:57:01 +0000

    basicarryadiscussion

---
+++ bascies1/myarray.js
@@ -0,0 +1,11 @@
+ const myheroes = ["Hanuman", "Krishna","Ram" ,"Shiv"]
+//  console.log(myheroes);
+  myheroes.push("lakhan")
+  console.log(myheroes)
+  
+ console.log(myheroes.slice(1,3))
+ console.log(myheroes)
+
+ console.log(myheroes.splice(1,3))
+ console.log(myheroes)
+ 
--- bascies1/numstring.js
+++ bascies1/numstring.js
@@ -4,6 +4,7 @@
 // console.log(firstname + "   " + lastname);
 // console.log(`myfirstname${firstname} is and my lastname${lastname}`);
 
+/*
 let email = "dg@live.com          "
 console.log(email)
 let emailTwo = "dg@mail.com"
@@ -11,3 +12,12 @@ console.log(emailTwo)
 console.log(emailTwo.replace('dg' , 'dhruv'));
 console.log(emailTwo.includes('dg'));
 console.log(email)
+*/
+
+const num = new Number(5.258)
+console.log(num.toFixed(2));
+
+const subscriber  = 10000000
+console.log (subscriber.toLocaleString());
+
+console.log(Math.floor(Math.random()*10+1))
